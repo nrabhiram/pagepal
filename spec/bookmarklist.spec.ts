9fc881dce458e5f85f41ce6ecac39847c150314e
Abhiram Reddy <nrabhiramr@gmail.com>
2023-02-15 20:13:50 +0530

    green build - bookmarklist add

---
+++ spec/bookmarklist.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+import { BookmarkList } from '../src/bookmarker/models/bookmarkList';
+import { Bookmark } from '../src/bookmarker/models/bookmark';
+import { Link } from '../src/bookmarker/models/link';
+import { Label } from '../src/bookmarker/models/label';
+
+describe('Bookmark List', () => {
+  it('A bookmark list with 0 bookmarks, upon adding a new bookmark, has 1 bookmark', () => {
+    const bookmarkList = new BookmarkList();
+    const bookmark = new Bookmark(new Link('https://test.com/'), new Label('Label'));
+    bookmarkList.add(bookmark);
+    expect(bookmarkList.bookmarks.length).toBe(1);
+  });
+
+  it('A bookmark list with 1 bookmark, upon adding a new bookmark, has it as the 2nd one in the list', () => {
+    const bookmarkList = new BookmarkList([new Bookmark(new Link('https://test1.com/'), new Label('Bookmark 1'))]);
+    const bookmark2 = new Bookmark(new Link('https://test2.com/'), new Label('Bookmark 2'));
+    bookmarkList.add(bookmark2);
+    expect(bookmarkList.bookmarks[1]).toEqual(bookmark2);
+  });
+});
---
+++ src/bookmarker/models/bookmarkList.ts
@@ -0,0 +1,17 @@
+import { Bookmark } from './bookmark';
+
+export class BookmarkList {
+  bookmarks: Bookmark[];
+
+  constructor(bookmarks?: Bookmark[]) {
+    if (bookmarks) {
+      this.bookmarks = bookmarks;
+    } else {
+      this.bookmarks = [];
+    }
+  }
+
+  add(bookmark: Bookmark) {
+    this.bookmarks.push(bookmark);
+  }
+}
