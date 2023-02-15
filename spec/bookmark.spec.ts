536f935975ce93c0d084e2e4fc45536c69be2fc6
Abhiram Reddy <nrabhiramr@gmail.com>
2023-02-15 14:23:43 +0530

    green build - edit link

--- spec/bookmark.spec.ts
+++ spec/bookmark.spec.ts
@@ -7,6 +7,13 @@ describe('Bookmark', () => {
     const bookmark = new Bookmark(Utils.buildLink('https://github.com/'), Utils.buildLabel('GitHub'));
     const editedBookmark = new Bookmark(Utils.buildLink('https://github.com/'), Utils.buildLabel('Github'));
     bookmark.edit(editedBookmark);
-    expect(bookmark).toEqual(editedBookmark);
+    expect(bookmark.label).toBe(editedBookmark.label);
+  });
+
+  it('A bookmark when edited with a new link, has its link updated to the edited one', () => {
+    const bookmark = new Bookmark(Utils.buildLink('https://github.com/'), Utils.buildLabel('Label'));
+    const editedBookmark = new Bookmark(Utils.buildLink('https://twitter.com/'), Utils.buildLabel('Label'));
+    bookmark.edit(editedBookmark);
+    expect(bookmark.link).toBe(editedBookmark.link);
   });
 });
