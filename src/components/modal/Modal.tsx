a6b12165d7086f86dcf48cdad5483638c7f990d6
Abhiram Reddy <nrabhiramr@gmail.com>
2023-02-22 10:11:08 +0530

    create portal for modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 
 export const Modal: React.FC<React.PropsWithChildren<{ open: boolean; onClose: () => void }>> = (props) => {
@@ -6,24 +7,27 @@ export const Modal: React.FC<React.PropsWithChildren<{ open: boolean; onClose: (
       props.onClose();
     }
   };
+
   return (
     <>
-      {props.open && (
-        <div className={styles['modal-container']} aria-labelledby="modal-title" role="dialog" aria-modal="true">
-          <div className={styles['overlay']}></div>
-          <div className={styles['modal-content']}>
-            <div
-              className={styles['content-container']}
-              id="modal-content-overlay"
-              onClick={closeModal}
-              onKeyDown={props.onClose}
-              role="presentation"
-            >
-              <div className={styles['content-card']}>{props.children}</div>
+      {props.open &&
+        createPortal(
+          <div className={styles['modal-container']} aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div className={styles['overlay']}></div>
+            <div className={styles['modal-content']}>
+              <div
+                className={styles['content-container']}
+                id="modal-content-overlay"
+                onClick={closeModal}
+                onKeyDown={props.onClose}
+                role="presentation"
+              >
+                <div className={styles['content-card']}>{props.children}</div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </div>,
+          document.getElementById('modal') as Element,
+        )}
     </>
   );
 };
